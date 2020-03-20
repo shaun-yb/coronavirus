fb47a72b1d986cf8cb5ad436770495106098dc83
Shaun Carland <shauncarland@gmail.com>
2020-03-20 16:59:09 -0400

    first commit

---
+++ lib/coronavirus.rb
@@ -0,0 +1,6 @@
+require "coronavirus/version"
+
+module Coronavirus
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/coronavirus/version.rb
@@ -0,0 +1,3 @@
+module Coronavirus
+  VERSION = "0.1.0"
+end
